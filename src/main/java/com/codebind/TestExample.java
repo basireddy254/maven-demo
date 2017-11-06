c58535cef1109773592c26bf10a1d1004196cc7e
Chinna Putha <chinna.putha@thomsonreuters.com>
2017-11-06 22:26:48 +0530

    Examle

---
+++ src/main/java/com/codebind/TestExample.java
@@ -0,0 +1,28 @@
+package com.codebind;
+
+import org.testng.annotations.Test;
+
+/**
+ * TestNG method will executed on alphabetical order
+ */
+public class TestExample {
+
+	@Test
+	public void btest1() {
+		System.out.println("TestExample.test1()");
+	}
+
+	@Test
+	public void ctest2() {
+		System.out.println("TestExample.test2()");
+	}
+	@Test
+	public void dtest3() {
+		System.out.println("TestExample.test3()");
+	}
+	@Test
+	public void atest4() {
+		System.out.println("TestExample.test4()");
+	}
+
+}
